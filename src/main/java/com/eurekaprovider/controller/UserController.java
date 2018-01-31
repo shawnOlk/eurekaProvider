0aa3ece571f0b4c7a3f4030258380293645a2fc4
sunlei <365243317@qq.com>
2018-01-31 14:52:05 +0800

    port

--- src/main/java/com/eurekaprovider/controller/UserController.java
+++ src/main/java/com/eurekaprovider/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.eurekaprovider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class UserController {
+    @Value("${server.port}")
+    String port;
     @ResponseBody
     @RequestMapping(value = "/name")
     public String getName() {
-        return "hello eureka!!!";
+        return "hello eureka!!!"+port;
     }
 }
